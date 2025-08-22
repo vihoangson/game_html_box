67e4a74862aae1d136d08ae4b9b5b9d8915f3bed
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2025-08-22 12:55:19 +0700

    update

--- js/config.js
+++ js/config.js
@@ -9,6 +9,11 @@ export const gameConfig = {
             gravity: { y: 300 },
             debug: false
         }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
     }
 };
-
