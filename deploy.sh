1cec38a1f85d22b545e987c63748f9c69bc5f7be
Vi Hoàng Sơn <vihoangson@gmail.com>
2025-08-23 10:00:30 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git add . && git commit -m "update" && git push origin -f && ssh root@oop.vn "cd /var/www/vhosts/fun.io.vn/public/GG && git pull"
+git add . && git commit -m "update" && git push origin -f && ssh root@188.166.213.71   "cd /var/www/vhosts/fun.io.vn/public/GG && git pull"
